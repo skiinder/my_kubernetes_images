daf56c631594d2ad5ee171830168451c4c05cb94
skiinder <skiinder@live.cn>
2022-07-27 12:53:32 +0800

    v1.8

--- hive/build.sh
+++ hive/build.sh
@@ -8,13 +8,13 @@ export MAVEN_HOME="${WORK_DIR}/apache-maven-${MVN_VERSION}"
 # pull hive source
 curl -L "https://dlcdn.apache.org/hive/hive-${HIVE_VERSION}/apache-hive-${HIVE_VERSION}-src.tar.gz" | tar zx && \
 cd "apache-hive-${HIVE_VERSION}-src" && \
-curl -L https://github.com/skiinder/hive/commit/394a38dd156c92479cc1a2a84c985b7ffc0afbb2.patch | git apply && \
-curl -L https://github.com/skiinder/hive/commit/782d5fbc04db32345905c64479a76a749747baeb.patch | git apply && \
+git apply "${WORK_DIR}/spark-3.patch" && \
+git apply "${WORK_DIR}/hive-19316.patch" && \
 
 # build hive
-cat pom.xml | grep '<spark.version>'
 "$MAVEN_HOME/bin/mvn" clean package  -Pdist -DskipTests -Dmaven.javadoc.skip=true && \
 tar zxf "packaging/target/apache-hive-${HIVE_VERSION}-bin.tar.gz" && \
 curl -L "https://repo.maven.apache.org/maven2/mysql/mysql-connector-java/5.1.49/mysql-connector-java-5.1.49.jar" \
      -o "apache-hive-${HIVE_VERSION}-bin/lib/mysql-connector-java.jar" && \
+cat pom.xml | grep '<spark.version>'
 mv "apache-hive-${HIVE_VERSION}-bin" "${WORK_DIR}/hive" || exit 1
