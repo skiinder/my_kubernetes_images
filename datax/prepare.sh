b5906be078cb5e3aadd3d48f09c947324229c2ea
skiinder <skiinder@live.cn>
2022-07-28 17:36:09 +0800

    datax moded

--- datax/prepare.sh
+++ datax/prepare.sh
@@ -2,6 +2,7 @@
 WORK_DIR="$(cd $(dirname "$0"); pwd)"
 cd "${WORK_DIR}"
 
+set -ex
 curl -L "https://datax-opensource.oss-cn-hangzhou.aliyuncs.com/${DATAX_VERSION}/datax.tar.gz" | tar -zx
 cd datax
 curl -L https://raw.githubusercontent.com/skiinder/my_kubernetes_images/main/scripts/filededup.sh | bash -
