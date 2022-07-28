935859e2defcddac16df96dd43e1cdd2fd57d460
skiinder <skiinder@live.cn>
2022-07-28 16:38:11 +0800

    ds moded

--- dolphinscheduler/prepare.sh
+++ dolphinscheduler/prepare.sh
@@ -9,4 +9,6 @@ curl -L "https://repo.maven.apache.org/maven2/io/juicefs/juicefs-hadoop/${JUICEF
 curl -L "https://repo.maven.apache.org/maven2/mysql/mysql-connector-java/5.1.49/mysql-connector-java-5.1.49.jar" \
      -o "apache-dolphinscheduler-${DOLPHINSCHEDULER_VERSION}-bin/lib/mysql-connector-java.jar"
 find "apache-dolphinscheduler-${DOLPHINSCHEDULER_VERSION}-bin" | grep 'sh$' | xargs -n1 sed -i '/^ *source/s/^/#/'
+cp "${WORK_DIR}/core-site.xml" "apache-dolphinscheduler-${DOLPHINSCHEDULER_VERSION}-bin/conf/core-site.xml"
+find "apache-dolphinscheduler-${DOLPHINSCHEDULER_VERSION}-bin" | grep 'logback-\(master\|worker\|api\)' | xargs -n1 sed -i '/<root level="INFO">/a<appender-ref ref="STDOUT"\/>'
 mv "apache-dolphinscheduler-${DOLPHINSCHEDULER_VERSION}-bin" "${WORK_DIR}/dolphinscheduler"
